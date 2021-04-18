192c8d76a609534fb22ae61dc3369b55ed7e43c2
ElijahAleakhue <elijah.aleakhue@thebulb.africa>
2021-04-18 18:22:43 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace example
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
